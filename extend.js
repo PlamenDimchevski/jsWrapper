c17cd4fa4ba2a7e482b6244937929ed872b739ae
Plamen Dimchevski <pdimchevske@gmail.com>
2014-05-19 15:01:21 +0300

    Initial extension mapping

--- extend.js
+++ extend.js
@@ -1,6 +1,48 @@
-Object.prototype.each = function () {
+var jsW = (function ( jsw ) {
 
-};
+   Object.defineProperties(Object.prototype, {
+      each : {
+         value : function () {
+         }
+      },
 
-Object.prototype.extend = function() {
-};
+      extend : {
+         value : function () {
+         }
+      },
+
+      append : {
+         value : function () {
+         }
+      }
+
+   });
+
+   Object.defineProperties(Array.prototype, {
+      each : {
+         value : function () {
+         }
+      },
+
+      extend : {
+         value : function () {
+         }
+      },
+
+      append : {
+         value : function () {
+         }
+      }
+   });
+
+   Object.defineProperties(Function.prototype, {
+
+      parent : {
+         value : function () {
+         }
+      }
+
+   });
+
+   return jsW;
+}( jsW || {} ));
